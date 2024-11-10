fa68357af7c9a993d32fc2890d0124659d43409e
Dylan Gyori <gyoridylan@gmail.com>
2024-11-09 23:16:50 -0800

    Created example of changing tones

--- ultrasonic_files/ultrasonic_files.cpp
+++ ultrasonic_files/ultrasonic_files.cpp
@@ -3,26 +3,103 @@
 
 #include <iostream>
 #include <SDL.h>
+#include <math.h>
+#include "FrequencyGenerator.h"
 
 using std::cout;
+
+struct WaveData
+{
+    int samplesPerSecond;
+    int bytesPerSample;
+    int tonePosition;
+    float toneHz;
+    float targetToneHz;
+    float toneVolume;
+    float wavePeriod;
+
+};
+
+void AudioCallback(void* UserData, Uint8* Stream, int Length)
+{
+    WaveData* AudioUserData = (WaveData*)UserData;
+    static Uint32 Count = 0;
+
+    Sint16* SampleBuffer = (Sint16*)Stream;
+    int SamplesToWrite = Length / AudioUserData->bytesPerSample;
+
+    // link target waveform to current waveform
+    if (AudioUserData->toneHz != AudioUserData->targetToneHz)
+    {
+        AudioUserData->tonePosition = AudioUserData->toneHz * AudioUserData->tonePosition / AudioUserData->targetToneHz;
+        AudioUserData->toneHz = AudioUserData->targetToneHz;
+    }
+
+    for (int i = 0; i < SamplesToWrite; ++i)
+    {
+        // number of samples to one cycle of the tone
+        float samplesPerCycle = float(AudioUserData->samplesPerSecond) / AudioUserData->toneHz;
+        Sint16 ToneValue = (0.5f + (AudioUserData->toneVolume * sin(2.0f * M_PI * AudioUserData->tonePosition / samplesPerCycle)));
+        SampleBuffer[i] = ToneValue;
+        
+        // advance in tone
+        ++(AudioUserData->tonePosition);
+    }
+
+    AudioUserData->toneHz = AudioUserData->targetToneHz;
+}
 
 int main(int argc, char* argv[])
 {
     if (SDL_Init(SDL_INIT_AUDIO) == -1)
+    {
         cout << SDL_GetError();
+        return 1;
+    }
 
     std::cout << "Hello World!\n";
+
+    WaveData AudioUserData = { 0 };
+    AudioUserData.samplesPerSecond = 48000;
+    AudioUserData.bytesPerSample = sizeof(Sint16);
+    AudioUserData.tonePosition = 0;
+    AudioUserData.toneVolume = 3000;
+    AudioUserData.toneHz = 840;
+    AudioUserData.targetToneHz = AudioUserData.toneHz;
+    
+
+    SDL_AudioSpec Want, Have;
+    SDL_AudioDeviceID AudioDeviceID;
+    Want.freq = AudioUserData.samplesPerSecond;
+    Want.format = AUDIO_S16;
+    Want.channels = 1;
+    // number of samples to process before processing change in tone
+    Want.samples = 4096;
+    Want.callback = &AudioCallback;
+    Want.userdata = &AudioUserData;
+    AudioDeviceID = SDL_OpenAudioDevice(0, 0, &Want, &Have, 0);
+
+    SDL_PauseAudioDevice(AudioDeviceID, 0);
+
+    int Running = 1;
+    while (Running)
+    {
+
+        SDL_Event Event;
+        while (SDL_PollEvent(&Event))
+        {
+            if (Event.type == SDL_QUIT)
+            {
+                Running = 0;
+                break;
+            }
+        }
+
+        AudioUserData.targetToneHz -= 25;
+
+        SDL_Delay(500);
+    }
 
     return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
